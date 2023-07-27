b504de70ff4d101cae4aeade2622794e9ff70a16
Cullen McClellan <cullen.mcclellan@mcullenm.dev>
2023-07-27 07:45:28 -0400

    Basic structure

---
+++ index.tsx
@@ -0,0 +1,28 @@
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html"
+import * as elements from "typed-html";
+
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) => html(
+  <BaseHtml>
+    <h1>This is JSX.</h1>
+  </BaseHtml>
+))
+  .listen(3000)
+
+console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+
+const BaseHtml = ({children}: elements.Children) => `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1.0>
+    <title>Bun Elysia HTMX</title>
+  </head>
+  <body>
+    ${children}
+  </body>
+`
